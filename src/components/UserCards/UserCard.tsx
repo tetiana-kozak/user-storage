d36a634b93390b92d26e076565f796d3d4d6c42c
Tetiana <kozaktanya077@gmail.com>
2023-06-05 22:47:50 +0300

    chore

--- src/components/UserCards/UserCard.tsx
+++ src/components/UserCards/UserCard.tsx
@@ -1,12 +1,12 @@
 import './UserCard.scss'
-import photo from '../../assets/photo-cover.svg'
+import photo1 from '../../assets/photo-cover.svg'
 
 type Props = {}
 
 const UserCard = (props: Props) => {
   return (
     <div className="user-card">
-      <img src={photo} alt="user photo" className="user-photo" />
+      <img src={photo1} alt="user photo" className="user-photo" />
       <h5 className="user-description">Takamaru Ayako Jurrien</h5>
       <div className="user-description">
         <h4>Lead Independent Director Lead Independent Director</h4>
