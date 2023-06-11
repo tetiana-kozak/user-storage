c89da07b528ea4b0cc8323b3983c48a5f70cce25
Tetiana <kozaktanya077@gmail.com>
2023-06-11 12:27:06 +0300

    fix: line clamp in user cards

--- src/components/UserCards/UserCard.tsx
+++ src/components/UserCards/UserCard.tsx
@@ -12,11 +12,11 @@ const UserCard = ({ name, email, phone, position, photo }: Props) => {
   return (
     <div className="user-card">
       <img src={photo} alt="user photo" className="user-photo" />
-      <h5 className="user-description">{name}</h5>
+      <h5 className="user-description line-clamp">{name}</h5>
       <div className="user-description">
-        <h4>{position}</h4>
-        <h4>{email}</h4>
-        <h4>{phone}</h4>
+        <h4 className="line-clamp">{position}</h4>
+        <h4 className="line-clamp">{email}</h4>
+        <h4 className="line-clamp">{phone}</h4>
       </div>
     </div>
   )
